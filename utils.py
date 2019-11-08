23ae060ce2fc502202bf1f56da2945167edc522c
Peter Yoachim <yoachim@uw.edu>
2019-11-08 13:53:39 -0800

    do this better with a class

--- utils.py
+++ utils.py
@@ -14,6 +14,8 @@ from pycraf import satellite
 from lsst.sims.utils import Site
 import skyfield.sgp4lib as sgp4lib
 from astropy.time import Time
+import ephem
+from lsst.sims.utils import _angularSeparation
 
 
 # adapting from:
@@ -31,8 +33,8 @@ def satellite_mean_motion(altitude, mu=const.GM_earth, r_earth=const.R_earth):
     return 1 / no
 
 
-def tle_from_orbital_parameters(
-        sat_name, sat_nr, epoch, inclination, raan, mean_anomaly, mean_motion):
+def tle_from_orbital_parameters(sat_name, sat_nr, epoch, inclination, raan,
+                                mean_anomaly, mean_motion):
     '''
     Generate TLE strings from orbital parameters.
 
@@ -66,7 +68,7 @@ def tle_from_orbital_parameters(
     return '\n'.join([tle0, tle1, tle2])
 
 
-def create_constellation(altitudes, inclinations, nplanes, sats_per_plane):
+def create_constellation(altitudes, inclinations, nplanes, sats_per_plane, epoch=22050.1, name='Test'):
 
     my_sat_tles = []
     sat_nr = 80000
@@ -88,7 +90,7 @@ def create_constellation(altitudes, inclinations, nplanes, sats_per_plane):
         for ma, raan in zip(mas, raans):
             my_sat_tles.append(
                 tle_from_orbital_parameters(
-                    'TEST {:d}'.format(sat_nr), sat_nr, 19050.1,
+                    name+' {:d}'.format(sat_nr), sat_nr, epoch,
                     inc, raan, ma, mm))
             sat_nr += 1
 
@@ -96,15 +98,17 @@ def create_constellation(altitudes, inclinations, nplanes, sats_per_plane):
 
 
 def starlink_constellation():
+    """
+    Create a list of satellite TLE's
+    """
     altitudes = np.array([550, 1110, 1130, 1275, 1325, 345.6, 340.8, 335.9]) * u.km
     inclinations = np.array([53.0, 53.8, 74.0, 81.0, 70.0, 53.0, 48.0, 42.0]) * u.deg
     nplanes = np.array([72, 32, 8, 5, 6, 2547, 2478, 2493])
     sats_per_plane = np.array([22, 50, 50, 75, 75, 1, 1, 1])
 
-    my_sat_tles = create_constellation(altitudes, inclinations, nplanes, sats_per_plane)
-    my_sats = [satellite.get_sat(sat_tle) for sat_tle in my_sat_tles]
+    my_sat_tles = create_constellation(altitudes, inclinations, nplanes, sats_per_plane, name='Starlink')
 
-    return my_sats
+    return my_sat_tles
 
 
 time_J2000 = datetime.datetime(2000, 1, 1, 12, 0)
@@ -151,4 +155,96 @@ def lsst_location():
     obs_loc_lsst = EarthLocation(lat=site.latitude, lon=site.longitude, height=site.height)
     sat_obs_lsst = satellite.SatelliteObserver(obs_loc_lsst)
     return sat_obs_lsst
+
+
+class Constellation(object):
+    """
+    Have a class to hold ephem satellite objects
+
+    Parameters
+    ----------
+    sat_tle_list : list of str
+        A list of satellite TLEs to be used
+    tstep : float (5)
+        The time step to use when computing satellite positions in an exposure
+    """
+
+    def __init__(self, sat_tle_list, alt_limit=30., fov=3.5, tstep=5., exptime=30.):
+        self.sat_list = [ephem.readtle(tle.split('\n')[0], tle.split('\n')[1], tle.split('\n')[2]) for tle in sat_tle_list]
+        self.alt_limit_rad = np.radians(alt_limit)
+        self.fov_rad = np.radians(fov)
+        self._make_observer()
+        self._make_fields()
+        self.tsteps = np.arange(0, exptime+tstep, tstep)/3600./24.  # to days
+
+    def _make_fields(self):
+        """
+        Make tesselation of the sky
+        """
+
+        # Read in the fields, build a kd-tree
+        pass
+
+    def _make_observer(self):
+        telescope = Site(name='LSST')
+
+        self.observer = ephem.Observer()
+        self.observer.lat = telescope.latitude_rad
+        self.observer.lon = telescope.longitude_rad
+        self.observer.elevation = telescope.height
+
+    def update_mjd(self, mjd):
+        """
+        observer : ephem.Observer object
+        """
+
+        self.observer.date = ephem.date(time.Time(mjd, format='mjd').datetime)
+
+        self.altitudes_rad = []
+        self.azimuth_rad = []
+        self.eclip = []
+        for sat in self.sat_list:
+            sat.compute(self.observer)
+            self.altitudes_rad.append(sat.alt)
+            self.azimuth_rad.append(sat.az)
+            self.eclip.append(sat.eclipsed)
+
+
+        self.altitudes_rad = np.array(self.altitudes_rad)
+        self.azimuth_rad = np.array(self.azimuth_rad)
+        self.eclip = np.array(self.eclip)
+        # Keep track of the ones that are up and illuminated
+        self.above_alt_limit = np.where((self.altitudes_rad >= self.alt_limit_rad) & (self.eclip == False))[0]
+
+    def fraction_covered(self, mjd):
+        """
+        Return the fraction of fields that would include a satellite at this time
+        """
+        mjds = mjd + self.tsteps
+
+        
+
+
+        pass
+
+    def check_pointing(self, pointing_alt, pointing_az, mjd):
+        """
+        See if a pointing has a satellite in it
+        """
+
+        mjds = mjd + self.tsteps
+        in_fov = 0
+
+        for mjd in mjds:
+            self.update_mjd(mjd)
+            ang_distances = _angularSeparation(self.azimuth_rad[self.above_alt_limit], self.altitudes_rad[self.above_alt_limit],
+                                               np.radians(pointing_az), np.radians(pointing_alt))
+            in_fov += np.size(np.where(ang_distances <= self.fov_rad)[0])
+        in_fov = in_fov/mjds.size
+        return in_fov
+
+
+
+
+
 
